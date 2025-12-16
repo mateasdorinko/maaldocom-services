6633bb1788ca21294bd42459908021e467f17105
mateasdorinko <matt@maaldo.com>
2025-12-16 16:26:33 -0600

    added default endpoint;

---
+++ src/MaaldoCom.Services.Api/Endpoints/DefaultEndpoint.cs
@@ -0,0 +1,28 @@
+﻿namespace MaaldoCom.Services.Api.Endpoints;
+
+public class DefaultEndpoint : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("/");
+        AllowAnonymous();
+        Options(x => x.ExcludeFromDescription());
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        const string htmlContent = """
+                                           <!DOCTYPE html>
+                                           <html>
+                                           <head>
+                                               <title>api.maaldo.com</title>
+                                           </head>
+                                           <body>
+                                               <h1>hi...</h1>
+                                           </body>
+                                           </html>
+                                   """;
+
+        await Send.StringAsync(htmlContent, contentType: "text/html", cancellation: ct);
+    }
+}
