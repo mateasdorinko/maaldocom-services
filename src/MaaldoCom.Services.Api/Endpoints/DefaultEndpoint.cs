31ab3212dc7c7f1fff816cf067f03278b7dc538b
Mateas Dorinko <matt@maaldo.com>
2025-12-23 13:12:02 -0600

    all the things;

--- src/MaaldoCom.Services.Api/Endpoints/DefaultEndpoint.cs
+++ src/MaaldoCom.Services.Api/Endpoints/DefaultEndpoint.cs
@@ -12,15 +12,24 @@ public class DefaultEndpoint : EndpointWithoutRequest
     public override async Task HandleAsync(CancellationToken ct)
     {
         const string htmlContent = """
-                                           <!DOCTYPE html>
-                                           <html>
-                                           <head>
-                                               <title>api.maaldo.com</title>
-                                           </head>
-                                           <body>
-                                               <h1>hi...</h1>
-                                           </body>
-                                           </html>
+                                   <!DOCTYPE html>
+                                   <html lang="">
+                                     <head>
+                                         <title>maaldo.com</title>
+                                         <style>
+                                             .container { display: flex; flex-direction: column; justify-content: flex-end; align-items: center; min-height: 85vh; }
+                                             .bottom-center-div { width: 50%; padding: 1em; text-align: center; font-size: x-small; }
+                                         </style>
+                                     </head>
+                                     <body>
+                                         <h1>hello...</h1>
+                                         <div class="container"> 
+                                             <div class="bottom-center-div">
+                                                 v2025.12.23a
+                                             </div>
+                                         </div>
+                                     </body>
+                                   </html>
                                    """;
 
         await Send.StringAsync(htmlContent, contentType: "text/html", cancellation: ct);
