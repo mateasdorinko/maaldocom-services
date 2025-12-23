0318b798927e03249c3f556b37edb2839d52ab3c
Mateas Dorinko <matt@maaldo.com>
2025-12-23 13:35:01 -0600

    renamed pipes from release to deploy to be more consistent with GH verbiage;

--- src/MaaldoCom.Services.Api/Endpoints/DefaultEndpoint.cs
+++ src/MaaldoCom.Services.Api/Endpoints/DefaultEndpoint.cs
@@ -25,7 +25,7 @@ public class DefaultEndpoint : EndpointWithoutRequest
                                          <h1>hello...</h1>
                                          <div class="container"> 
                                              <div class="bottom-center-div">
-                                                 v2025.12.23d
+                                                 v2025.12.23e
                                              </div>
                                          </div>
                                      </body>
