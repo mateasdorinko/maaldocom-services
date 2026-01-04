5043400e7778df0a4a4c20cffd3fb379a2858e5d
Mateas Dorinko <matt@maaldo.com>
2026-01-04 03:54:25 -0600

    resolved final sonarqube issue;

--- src/MaaldoCom.Services.Application/Errors/ValidationFailureError.cs
+++ src/MaaldoCom.Services.Application/Errors/ValidationFailureError.cs
@@ -1,3 +1,20 @@
 namespace MaaldoCom.Services.Application.Errors;
 
-public class ValidationFailureError { }
+public class ValidationFailureError<TEntity> : IError where TEntity : BaseEntity
+{
+    public ValidationFailureError(TEntity entity)
+    {
+        var entityTypeName = entity?.GetType().Name ?? typeof(TEntity).Name;
+        Message = $"{entityTypeName} validation error(s).";
+        Metadata = new Dictionary<string, object>
+        {
+            { "EntityType", entityTypeName },
+            { "Entity", entity! }
+        };
+    }
+
+    public string Message { get; }
+    public Dictionary<string, object> Metadata { get; }
+
+    public List<IError> Reasons { get; } = [];
+}
