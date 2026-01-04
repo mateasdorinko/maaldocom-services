2480dc991b30827940de6b74d9b3d838b670342d
Mateas Dorinko <matt@maaldo.com>
2026-01-04 00:07:14 -0600

    More unit tests;

--- tests/Tests.Unit.Application/Queries/Knowledge/GetRandomKnowledgeQueryHandlerTests/ExecuteAsync.cs
+++ tests/Tests.Unit.Application/Queries/Knowledge/GetRandomKnowledgeQueryHandlerTests/ExecuteAsync.cs
@@ -1,16 +1,39 @@
-﻿namespace Tests.Unit.Application.Queries.Knowledge.GetRandomKnowledgeQueryHandlerTests;
+﻿using MaaldoCom.Services.Application.Queries.Knowledge;
+
+namespace Tests.Unit.Application.Queries.Knowledge.GetRandomKnowledgeQueryHandlerTests;
 
 public class ExecuteAsync
 {
-    [Fact(Skip = "Not implemented yet")]
-    public async Task ExecuteAsync_x_y()
+    [Fact]
+    public async Task ExecuteAsync_Invoked_ReturnsRandomizedKnowledge()
     {
         // arrange
+        var user = A.Fake<ClaimsPrincipal>();
+        var cacheManager = A.Fake<ICacheManager>();
+        var ct = CancellationToken.None;
+
+        var knowledgeList = new List<KnowledgeDto>
+        {
+            new() { Id = Guid.NewGuid(), Title = "title1", Quote = "quote1" },
+            new() { Id = Guid.NewGuid(), Title = "title2", Quote =  "quote2" },
+            new() { Id = Guid.NewGuid(), Title = "title3", Quote =  "quote3" }
+        };
+
+        var query = new GetRandomKnowledgeQuery(user);
+        var handler = new GetRandomKnowledgeQueryHandler(cacheManager);
+
+        A.CallTo(() => cacheManager.ListKnowledgeAsync(ct)).Returns(knowledgeList);
 
         // act
-        await Task.CompletedTask;
+        var result = await handler.ExecuteAsync(query, ct);
+
+        var matchedKnowledge = knowledgeList.FirstOrDefault(k =>
+            k.Id == result.Value.Id &&
+            k.Title == result.Value.Title &&
+            k.Quote == result.Value.Quote);
 
         // assert
-        Assert.True(true);
+        result.IsSuccess.ShouldBe(true);
+        matchedKnowledge.ShouldNotBeNull();
     }
 }
--- tests/Tests.Unit.Application/Queries/Knowledge/ListKnowledgeQueryHandlerTests/ExecuteAsync.cs
+++ tests/Tests.Unit.Application/Queries/Knowledge/ListKnowledgeQueryHandlerTests/ExecuteAsync.cs
@@ -1,16 +1,34 @@
-﻿namespace Tests.Unit.Application.Queries.Knowledge.ListKnowledgeQueryHandlerTests;
+﻿using MaaldoCom.Services.Application.Queries.Knowledge;
+
+namespace Tests.Unit.Application.Queries.Knowledge.ListKnowledgeQueryHandlerTests;
 
 public class ExecuteAsync
 {
-    [Fact(Skip = "Not implemented yet")]
+    [Fact]
     public async Task ExecuteAsync_x_y()
     {
         // arrange
+        var user = A.Fake<ClaimsPrincipal>();
+        var cacheManager = A.Fake<ICacheManager>();
+        var ct = CancellationToken.None;
+
+        var knowledgeList = new List<KnowledgeDto>
+        {
+            new() { Id = Guid.NewGuid(), Title = "title1", Quote = "quote1" },
+            new() { Id = Guid.NewGuid(), Title = "title2", Quote =  "quote2" },
+            new() { Id = Guid.NewGuid(), Title = "title3", Quote =  "quote3" }
+        };
+
+        var query = new ListKnowledgeQuery(user);
+        var handler = new ListKnowledgeQueryHandler(cacheManager);
+
+        A.CallTo(() => cacheManager.ListKnowledgeAsync(ct)).Returns(knowledgeList);
 
         // act
-        await Task.CompletedTask;
+        var result = await handler.ExecuteAsync(query, ct);
 
         // assert
-        Assert.True(true);
+        result.IsSuccess.ShouldBe(true);
+        result.Value.Count().ShouldBe(3);
     }
 }
