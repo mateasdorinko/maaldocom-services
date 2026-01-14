72f42b4782787c2db999d09e64fd2e9fcc4d124e
mateasdorinko <matt@maaldo.com>
2026-01-14 11:29:09 -0600

    Added openapi codegen for Cli project via Refit;

--- src/MaaldoCom.Services.Cli/Program.cs
+++ src/MaaldoCom.Services.Cli/Program.cs
@@ -1,33 +1,13 @@
-/*
-using MaaldoCom.Services.Cli.Commands;
+using MaaldoCom.Services.Cli;
 using MaaldoCom.Services.Cli.Infrastructure;
-using MaaldoCom.Services.Cli.MaaldoApi;
 using Microsoft.Extensions.DependencyInjection;
 using Refit;
+using System.Text.Json;
 using Spectre.Console.Cli;
 
 var services = new ServiceCollection();
-
-services.AddRefitClient<IMaaldoApiClient>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://app-maaldocomapi-tst-cus.azurewebsites.net"));
-
 var registrar = new TypeRegistrar(services);
 var app = new CommandApp(registrar);
-
-app.Configure(config =>
-{
-    config.AddCommand<KnowledgeCommand>("knowledge")
-        .WithDescription("List knowledge items from the API");
-});
-
-return await app.RunAsync(args);
-*/
-
-using System.Text.Json;
-using Microsoft.Extensions.DependencyInjection;
-using Refit;
-
-var services = new ServiceCollection();
 
 var options = new JsonSerializerOptions
 {
@@ -41,4 +21,17 @@ services.AddRefitClient<IMaaldoApiClient>(new RefitSettings
     })
     .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://api.maaldo.com"));
 
-public interface IMaaldoApiClient { }
+var client = RestService.For<IMaaldoApiClient>("https://app-maaldocomapi-tst-cus.azurewebsites.net");
+
+var response = await client.GetRandomKnowledge();
+Console.WriteLine($"Random Knowledge: {response.Title} - {response.Quote}");
+
+/*
+app.Configure(config =>
+{
+    config.AddCommand<KnowledgeCommand>("knowledge")
+        .WithDescription("List knowledge items from the API");
+});
+
+return await app.RunAsync(args);
+*/
